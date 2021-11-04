0dadd3096a24c0c8ad5a4af1318f1b9ab7dc8552
LaoshuBaby <keaitianxinmail@qq.com>
2021-11-04 23:04:04 +0800

    Update main.py
    github.com/github/copilot-docs
    
    这玩意真好用

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 import uuid
 
-mat = []
-for i in range(5):
-    mat.append([0, 0, 0, 0, 0])
 
-# print(mat)
+def identicon_gen(username):
+    icon_uuid = str(uuid.uuid5(uuid.NAMESPACE_DNS, username))
+    icon_colour = icon_uuid[0:6]
+    print(icon_colour)
+    icon_style = icon_uuid[6:7]
+    print(icon_style)
+    icon_mat_str = icon_uuid[7:8] + icon_uuid[9:13] + icon_uuid[14:18] + icon_uuid[19:23] + icon_uuid[24:]
+    print(icon_mat_str)
 
-# 从UUID5生成identicon，用库画出来
-username = input()
-uuid_result = str(uuid.uuid5(uuid.NAMESPACE_DNS, username))
-print(uuid_result)
+    icon_mat = [[0] * 5] * 5
 
-uuid_result_colour = uuid_result[0:6]
-print(uuid_result_colour)
-uuid_result_style = uuid_result[6:7]
-print(uuid_result_style)
-uuid_result_01 = uuid_result[7:8] + uuid_result[9:13] + uuid_result[14:18] + uuid_result[19:23] + uuid_result[24:]
-print(uuid_result_01)
+    for i in range(5):
+        for j in range(5):
+            icon_mat[i][j] = str(icon_mat_str[i * 5 + j])
 
-# 需要从LSN的库里面偷一部分tkinter和rgb的库
+    # 需要从LSN的库里面偷一部分tkinter和rgb的库
 
-style_dict = {
-    "0": "default",
-    "1": "default",
-    "2": "default",
-    "3": "default",
-    "4": "default",
-    "5": "default",
-    "6": "default",
-    "7": "default",
-    "8": "default",
-    "9": "default",
-    "a": "default",
-    "b": "default",
-    "c": "default",
-    "d": "default",
-    "e": "default"
-}
+    return [icon_colour, icon_style, icon_mat]
+
+
+def identicon_print(icon_colour, icon_style, icon_mat):
+    style_dict = {
+        "0": "default",
+        "1": "default",
+        "2": "default",
+        "3": "default",
+        "4": "default",
+        "5": "default",
+        "6": "default",
+        "7": "default",
+        "8": "default",
+        "9": "default",
+        "a": "default",
+        "b": "default",
+        "c": "default",
+        "d": "default",
+        "e": "default"
+    }
+    pass
+
+
+if __name__ == "__main__":
+    print(identicon_gen("test"))
